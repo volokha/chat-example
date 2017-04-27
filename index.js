deaf38139e4c5d406e3faefb904bcbf6e6566f99
Penyok8 <kooostya1@gmail.com>
2017-04-27 17:31:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+   console.log('a user connected');
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', msg);    
   });
 });
 
